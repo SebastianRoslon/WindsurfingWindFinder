f658de22f536de5c20f91356f86d5c60ad7fe4c9
Sebastian Roslon <sebekk7@gmail.com>
2024-01-23 18:22:09 +0100

    Initial commit

---
+++ src/main/java/pl/roslon/WindsurfingWindFinder/WindsurfingWindFinderApplication.java
@@ -0,0 +1,13 @@
+package pl.roslon.WindsurfingWindFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfingWindFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfingWindFinderApplication.class, args);
+	}
+
+}
